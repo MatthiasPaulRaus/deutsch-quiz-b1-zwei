bf8883b14576b8ce8ee1534bcc454af1afc4621a
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-04-09 20:39:10 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+import QuestionsList from "./components/QuestionsList"
 import './App.css';
+import {v4 as uuidv4} from 'uuid';
 
-function App() {
+const App =()=> {
+
+  const [currentQuestion, setCurrentQuestion]= useState(0);
+  const [score, setScore]= useState(0)
+  const [clicked, setClicked] = useState(false);
+  const [showScore, setShowScore]= useState(false);
+  const [valid, setValid]= useState(false)
+
+  const handleCorrectAnswer = (isCorrect) => {
+    if(isCorrect) {
+      setScore(score + 1);
+      setValid(true)
+    }
+    setClicked(true);
+    
+  }
+
+  const handleNextQuestion = () => {
+    setClicked(false);
+    setValid(false)
+    if(currentQuestion < QuestionsList.length -1){
+      setCurrentQuestion(currentQuestion +1)
+    }
+    else{
+      setShowScore(true);
+    }
+  }
+
+  const playAgain = ()=> {
+    window.location.reload()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+
+      {showScore ? (
+        <div>
+          <div className="completed">Ende!</div>
+
+          <div className="score-section">
+            Punkte: {score}/{QuestionsList.length}
+          </div>
+
+          <button className="next-button"
+          onClick={playAgain}
+          >Spiel wiederholen</button>
+
+           
+          </div>
+        ) : (
+      
+        <div className="question-section-wrapper">
+
+          <div className="question-count">
+           Aufgabe {currentQuestion + 1} von {QuestionsList.length}
+          </div>
+
+          <div className="question">
+            {QuestionsList[currentQuestion].question}
+          </div>
+
+          <div className="answer-section-wrapper">
+            {QuestionsList[currentQuestion].answersList.map((answerOption) => (
+              <li className="answer-list" key={uuidv4()}>
+                <button 
+                disabled={clicked}
+                className={`answer-button
+                 ${clicked && answerOption.isCorrect ? "correct" : null}`}
+
+                onClick={() => handleCorrectAnswer(answerOption.isCorrect)}
+                >
+                {answerOption.answer} 
+                </button>
+              </li>
+            ))}
+          </div>
+
+          {clicked && valid &&
+          <div className="richtig"
+          >richtig</div>
+          }
+
+          {clicked && !valid &&
+          <div className="falsch"
+          >falsch</div>
+          }
+          <div>
+            <button className="next-button"
+            onClick={handleNextQuestion}
+            disabled={!clicked}
+            >weiter</button>
+          </div>
+      
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/components/QuestionsList.js
@@ -0,0 +1,225 @@
+const QuestionsList = [
+    {
+    question: "Für welche Berufe interessieren Sie sich?",
+    answersList: [
+        {answer: "Soziale Berufe finde ich interessant.", isCorrect:true},
+        {answer: "Ich bin Sozialarbeiter.", isCorrect:false},  
+        {answer: "Ich bin technisch nicht begabt.", isCorrect:false},
+        ]  
+    },
+    {
+    question: "Welche Berufe entsprechen Ihren Fähigkeiten?",
+    answersList: [
+        {answer:"Ich habe soziale Fähigkeiten.", isCorrect:false},
+        {answer:"Ich bin technisch sehr begabt.", isCorrect:false},
+        {answer:"Technische Berufe passen gut zu mir.", isCorrect:true},
+        ]   
+    },
+    
+    {
+    question:"Ich bin gern mit Menschen zusammen.",
+    answersList: [
+        {answer:"Dann solltest du nicht im Team arbeiten.", isCorrect:false},
+        {answer:"Dann solltest du einen Sozialberuf wählen.", isCorrect:true},
+        {answer:"Du könntest nachts arbeiten.", isCorrect:false},
+        ]    
+    },
+    
+    {question:"Regelmäßig Sport zu treiben",
+    answersList: [
+        {answer:"trägt zu einem besseren Wohlbefinden bei.", isCorrect:true},
+        {answer:"schadet Ihrem Wohlbefinden.", isCorrect:false},
+        {answer:"stärkt das Wohlfühlen", isCorrect:false},
+        ]    
+    },
+    {question:"Sie können Ihr persönliches Fitnessprogramm",
+    answersList: [
+        {answer:"herstellen.", isCorrect:false},
+        {answer:"vorstellen.", isCorrect:false},
+        {answer:"zusammenstellen.", isCorrect:true},
+        ]    
+    },
+    {question:"Unsere Fitnessprogramme",
+    answersList: [
+        {answer:"finden sie im Freien.", isCorrect:false},
+        {answer:"finden ausschließlich im Freien statt.", isCorrect:true},
+        {answer:"sind ein Risiko für die Gesundheit.", isCorrect:false},
+        ]    
+    },
+    {question:"Die Arbeit im Homeoffice macht unser Unternehmen",
+    answersList: [
+        {answer:"familien-freundlicher.", isCorrect:true},
+        {answer:"familiär.", isCorrect:false},
+        {answer:"kinderfreundlich.", isCorrect:false},
+        ]    
+    },
+    {question:"Hätten Sie bei der Arbeit gern längere Pausen?",
+    answersList: [
+        {answer:"Ja, gern. Dann kann ich länger arbeiten.", isCorrect:false},
+        {answer:"Klar. Kürzere Pausen sind besser.", isCorrect:false},
+        {answer:"Nein, eine kürzere Arbeitszeit ist mir lieber.", isCorrect:true},
+        ]    
+    },
+    {question:"Die Mahlzeiten unserer Kantine",
+    answersList: [
+        {answer:"sind allergisch für die Gesundheit.", isCorrect:false},
+        {answer:"sind auch für Allergiker geeignet.", isCorrect:true},
+        {answer:"sind auch für Vegetarier allergisch.", isCorrect:false},
+        ]    
+    },
+    {question:"Sport trägt zu einem größeren ... bei.",
+    answersList: [
+        {answer:"Wohlfühlen", isCorrect:false},
+        {answer:"Fitnessangebot", isCorrect:false},
+        {answer:"Wohlbefinden", isCorrect:true},
+        ]    
+    },
+
+
+    {question:"Was machen Sie nach der Arbeit?",
+    answersList: [
+        {answer:"Bevor ich nach Hause fahre, gehe ich zum Training.", isCorrect:true},
+        {answer:"Nach dem Training, fahre ich zur Arbeit.", isCorrect:false},
+        {answer:"Bevor ich trainiere, muss ich arbeiten. ", isCorrect:false},
+        ]    
+    },
+    {question:"Was machen Sie vor der Arbeit?",
+    answersList: [
+        {answer:"Während ich arbeite, lese ich die E-Mails.", isCorrect:false},
+        {answer:"Nach dem Aufstehen frühstücke ich.", isCorrect:true},
+        {answer:"Bevor ich einkaufe, gehe ich zur Arbeit.", isCorrect:false},
+        ]    
+    },
+    {question:"Ich bin fertig mit meinem Vortrag.",
+    answersList: [
+        {answer:"Vielen Dank für Ihre Aufmerksamkeit.", isCorrect:true},
+        {answer:"Danke für Ihre Einleitung.", isCorrect:false},
+        {answer:"Und nun komme ich zum nächsten Punkt.", isCorrect:false},
+        ]    
+    },
+    {question:"Der Inhalt meiner Präsentation:",
+    answersList: [
+        {answer:"Abschließend stehe ich für Fragen zur Verfügung.", isCorrect:false},
+        {answer:"Zuerst möchte ich über ... sprechen.", isCorrect:true},
+        {answer:"Anschließend gehe ich auf Fragen ein.", isCorrect:false},
+        ]    
+    },
+    {question:"Unsere Produkte sind Handarbeit, ",
+    answersList: [
+        {answer:"denn sie sind teurer als Industrieprodukte.", isCorrect:false},
+        {answer:"obwohl sie industriell hergestellt werden.", isCorrect:false},
+        {answer:"deshalb kosten sie mehr als Industriewaren.", isCorrect:true},
+        ]    
+    },
+    {question:"Schon vor dem Studium ",
+    answersList: [
+        {answer:"wusste ich, was ich werden wollte.", isCorrect:true},
+        {answer:"wusste ich nicht, welches Handwerk ich lernen wollte.", isCorrect:false},
+        {answer:"war mir klar, dass ich nicht studieren wollte.", isCorrect:false},
+        ]    
+    },
+    {question:"Am wichtigsten ist es",
+    answersList: [
+        {answer:"dem Käufer zu widersprechen.", isCorrect:false},
+        {answer:"die Kunden zu überzeugen.", isCorrect:true},
+        {answer:"den Interessenten eine Aufgabe zu geben.", isCorrect:false},
+        ]    
+    },
+    {question:"Ein attraktiver Arbeitgeber ist",
+    answersList: [
+        {answer:"jemand, der gute Aufstiegschancen bietet.", isCorrect:true},
+        {answer:"jemand, der schöne Arbeitsplätze schafft.", isCorrect:false},
+        {answer:"jemand, der weiß, was die Mitarbeiter wünschen.", isCorrect:false},
+        ]    
+    },
+    {question:"Ein gutes Betriebsklima",
+    answersList: [
+        {answer:"hat ein angemessenes Gehalt.", isCorrect:false},
+        {answer:"sieht man am Wohlbefinden der Mitarbeiter.", isCorrect:true},
+        {answer:"bietet attraktive Weiterbildungsangebote.", isCorrect:false},
+        ]    
+    },
+    {question:"Wenn wir es früher gewusst hätten,",
+    answersList: [
+        {answer:"hätten wir den Fehler nicht gemacht.", isCorrect:true},
+        {answer:"hätten Sie uns ausführlich informiert.", isCorrect:false},
+        {answer:"hätten sie es uns erklärt.", isCorrect:false},
+        ]    
+    },
+
+
+    {question:"Wären wir doch früher losgefahren.",
+    answersList: [
+        {answer:"Dann wären wir zu spät gekommen.", isCorrect:false},
+        {answer:"Dann wären wir nicht angekommen.", isCorrect:false},
+        {answer:"Dann würden wir jetzt nicht im Stau stehen.", isCorrect:true},
+        ]    
+    },
+    {question:"Nachdem wir ein ganzes Jahr lang trainiert hatten,",
+    answersList: [
+        {answer:"gewannen wir das wichtige Turnier.", isCorrect:true},
+        {answer:"wollen wir endlich das Turnier gewinnen.", isCorrect:false},
+        {answer:"gewinnen wir alle Turniere.", isCorrect:false},
+        ]    
+    },
+    {question:"Das ist mir auch schon passiert.",
+    answersList: [
+        {answer:"Sie können es sich denken.", isCorrect:false},
+        {answer:"Ich kann Sie sehr gut verstehen.", isCorrect:true},
+        {answer:"Wirklich? Ich kann es nicht glauben.", isCorrect:false},
+        ]    
+    },
+    {question:"Darüber hätte ich mich auch geärgert.",
+    answersList: [
+        {answer:"Worüber hast du dich geärgert?", isCorrect:false},
+        {answer:"Warum haben Sie sich so aufgeregt?", isCorrect:false},
+        {answer:"Mir war das auch passiert.", isCorrect:true},
+        ]    
+    },
+    {question:"Was feiern die Leute da auf dem Platz?",
+    answersList: [
+        {answer:"Das sollte das Maifest sein.", isCorrect:false},
+        {answer:"Das müsste das Sommerfest sein", isCorrect:true},
+        {answer:"Das war der Weihnachtsmarkt.", isCorrect:false},
+        ]    
+    },
+    {question:"Vielen Dank für Ihre Einladung.",
+    answersList: [
+        {answer:"Aber leider haben wir schon etwas vor.", isCorrect:true},
+        {answer:"Bevor wir kommen, brauchen wir eine Einladung.", isCorrect:false},
+        {answer:"Das Jubiläum haben wir mit allen Kollegen gefeiert.", isCorrect:false},
+        ]    
+    },
+    {question:"Das Filmfest dieses Jahres war ein voller Erfolg.",
+    answersList: [
+        {answer:"Letztes Jahr war ein großes Fest.", isCorrect:false},
+        {answer:"Dieses Jahr war er nicht erfolgreich.", isCorrect:false},
+        {answer:"Nächstes Jahr wird es noch besser.", isCorrect:true},
+        ]    
+    },
+    {question:"Trotz des schlechten Wetters",
+    answersList: [
+        {answer:"konnte das Seminar im Freien stattfinden.", isCorrect:true},
+        {answer:"kann die Vorstellung nicht stattfinden.", isCorrect:false},
+        {answer:"war die Veranstaltung kein Erfolg.", isCorrect:false},
+        ]    
+    },
+    {question:"Das Verhalten der Kollegen war etwas merkwürdig.",
+    answersList: [
+        {answer:"Ich konnte es mir nicht merken.", isCorrect:false},
+        {answer:"Ja, sie waren sehr freundlich.", isCorrect:false},
+        {answer:"Ja, das ist mir auch aufgefallen.", isCorrect:true},
+        ]    
+    },
+    {question:"Sie sollten dringend aufhören zu rauchen!",
+    answersList: [
+        {answer:"Womit soll ich aufhören?", isCorrect:true},
+        {answer:"Wem soll ich zuhören?", isCorrect:false},
+        {answer:"Wozu sollte ich rauchen?", isCorrect:false},
+        ]    
+    },
+    
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
